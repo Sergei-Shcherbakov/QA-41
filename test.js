6061d33163b31bc6296dc5c362640ed73b8406bd
Студент <@Top.Academy>
2024-06-13 18:39:35 +0300

    1

---
+++ test.js
@@ -0,0 +1,47 @@
+     // alert("Hellow world") // cтрока
+        // alert(5/0) //infinity
+        // alert("lala"/5) //Nan (Not a nomber)
+        // null - пустота
+        // underfinded не назначенно/ не заданно
+        // let name = "Sergei"
+        // alert ("Привет," + name + " !");
+        // alert (`Привет,${name} !`);
+       
+    //    prompt ('Text');
+    //    let name = prompt("Введите ваше имя: ");
+    //    alert (`Привет,${name} !`);
+    //    confirm (`Ты ${name}?!`);
+    //  let a = parseInt( prompt('Введите а: '));
+    //  let b = parseInt(prompt('Введите b: '));
+    // alert (a + b);
+    //  let number = parseInt(prompt('Введите возраст: '))
+    //  if (number >= 18) {alert('Nice')}
+    //  else {alert('Not Nice')}
+    // let accessAllowed = (age > 20) ? true: false;
+    // let year = prompt('Сколько вам лет?');
+
+    // let result = (year > 20) ? alert('Super'): alert ("beeeeeeeeeeee");
+
+    // || - или
+    // && -и 
+    // != - не равно
+    // if(year == 20 || year == 21){
+    // alert('Super!')
+    // }
+
+    // let i = 0;
+    // while (i<5)
+    // {
+    //     alert(i);
+    //     i++;
+    // }
+    // for (let i = 0; i < 5; i++) {
+    //     alert(i);
+        
+    // }
+    // let name = prompt('Введите ваше имя: ')
+    // function funk (){
+    //     alert(`Приыет,${name}!`);
+    // }
+    // funk();
+    alert ("Ghfgjkdhkfjdlkf");
